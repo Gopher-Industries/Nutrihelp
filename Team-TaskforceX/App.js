7a49096fff2015b49f9a718e221b91adad1ebb8c
AshleyDowie <67585778+AshleyDowie@users.noreply.github.com>
2022-12-01 20:48:12 +1100

    198-Change-Working-Code-Folder-In-Repository
    - Removed folder TTFX_T3_2022
    - Added project provided by Neym, found in "Team-TaskforceX"

---
+++ Team-TaskforceX/App.js
@@ -0,0 +1,160 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+//StackScreens
+import SplashScreen from './src/screens/SplashScreen.js';
+import ConfirmPrefScreen from './src/screens/ConfirmPrefScreen.js';
+import LoginScreen from './src/screens/LoginScreen.js';
+import TermScreen from './src/screens/TermScreen.js';
+import CreateAccountScreen from './src/screens/CreateAccountScreen.js';
+import CreateAccountScreen2 from './src/screens/CreateAccountScreen2.js';
+import CreateProfileScreen from './src/screens/CreateProfileScreen.js';
+import DietRequirementScreen from './src/screens/DietRequirementScreen.js';
+import AllergyScreen from './src/screens/AllergyScreen.js';
+import DislikesScreen from './src/screens/DislikesScreen.js';
+import HealthConditionScreen from './src/screens/HealthConditionScreen.js';
+import DailyNutritionPlanScreen from './src/screens/DailyNutritionPlanScreen.js';
+import MealPlanScreen from './src/screens/MealPlanScreen.js';
+
+//TabScreens
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/screens/HomeScreen.js';
+import GroceriesScreen from './src/screens/GroceriesScreen.js';
+import UnnamedScreen from './src/screens/UnnamedScreen.js';
+import PreferenceScreen from './src/screens/PreferenceScreen.js';
+
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      // screenOption={{
+      //   tabBarActiveTintColor: 'pink',
+      // }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Groceries"
+        component={GroceriesScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="shopping-cart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Unnamed"
+        component={UnnamedScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="cutlery" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Preference"
+        component={PreferenceScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="user-circle-o" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+//change headerShown to true for sanity check
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="TermScreen"
+          component={TermScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="CreateAccountScreen"
+          component={CreateAccountScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="CreateAccountScreen2"
+          component={CreateAccountScreen2}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="CreateProfileScreen"
+          component={CreateProfileScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="DietRequirementScreen"
+          component={DietRequirementScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="AllergyScreen"
+          component={AllergyScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="DislikesScreen"
+          component={DislikesScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="HealthConditionScreen"
+          component={HealthConditionScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="ConfirmPrefScreen"
+          component={ConfirmPrefScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="DailyNutritionPlanScreen"
+          component={DailyNutritionPlanScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="MealPlanScreen"
+          component={MealPlanScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Tabs"
+          component={Tabs}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ Team-TaskforceX/src/screens/CreateAccountScreen2.js
@@ -0,0 +1,189 @@
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const CreateAccountScreen2 = () => {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [emailConfirm, setEmailConfirm] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfrim, setPasswordConfirm] = useState('');
+  const [checkValidEmail, setCheckValidEmail] = useState('');
+
+  const handleCheckEmail = val => {
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+
+    if (val.length === 0) {
+      setCheckValidEmail('Email address must be entered');
+    } else if (reg.test(val) === false) {
+      setCheckValidEmail('Please enter valid email address');
+    } else if (reg.test(val) === true) {
+      setCheckValidEmail('');
+    }
+  };
+
+  const handleEmailConfirm = val => { // check if email are same 
+    if (val != email) {
+      setCheckValidEmail('Email does not match');
+    } else {
+      setCheckValidEmail('');
+    }
+  };
+
+  const handlePassConfirm = val => { // check if pass are same 
+    if (val != password) {
+      setCheckValidEmail('Pass does not match');
+    } else {
+      setCheckValidEmail('');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Icon
+        name="arrow-left"
+        size={20}
+        color="black"
+        type="entypo"
+        onPress={() => navigation.goBack()}
+      />
+      <View>
+        <Text style={styles.title}>Create Account</Text>
+      </View>
+      <View style={styles.inputView}> 
+      {/* Enter Email */}
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email address*"
+          placeholderTextColor="gray"
+          keyboardType="email-address"
+          onChangeText={value => {
+            setEmail(value);
+            handleCheckEmail(value);
+          }}
+        />
+      </View>
+
+      {/* Validation text */}
+      {checkValidEmail ? (
+        <Text style={styles.emailValidationText}>{checkValidEmail}</Text>
+      ) : null} 
+      {/* Confirm Email  */}
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Confirm Email Address*"
+          placeholderTextColor="gray"
+          keyboardType="email-address"
+          onChangeText={emailConfirm => {
+          setEmailConfirm(emailConfirm);
+          handleEmailConfirm(emailConfirm);
+          }}
+          />
+      </View>
+      <View style={styles.inputView}>
+        {/* Enter Password */}
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password*"
+          placeholderTextColor="gray"
+          secureTextEntry={true}
+          onChangeText={password => 
+            setPassword(password)
+          }
+        />
+      </View>
+      <View>
+        <Text style={styles.text}>Password must have...</Text>
+      </View>
+      
+      <View style={styles.inputView}>
+        {/* Password Confirm */}
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Confirm Password*"
+          placeholderTextColor="gray"
+          secureTextEntry={true}
+          onChangeText={password => {
+            setPasswordConfirm(password);
+            handlePassConfirm(password);
+          }}
+        />
+      </View>
+      <View>
+        <Text style={styles.text}>* Mandatory information</Text>
+      </View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('CreateProfileScreen')}>
+        <Text style={styles.buttonText}>Continue</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default CreateAccountScreen2;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 30,
+  },
+
+  title: {
+    fontSize: 25,
+    color: 'black',
+    marginBottom: 20,
+    marginTop: 20,
+  },
+
+  inputView: {
+    borderRadius: 5,
+    borderColor: 'black',
+    height: 60,
+    marginBottom: 20,
+    borderWidth: 1,
+  },
+
+  TextInput: {
+    height: 50,
+    flex: 1,
+    marginLeft: 10,
+    fontSize: 15,
+  },
+
+  text: {
+    fontWeight: 'bold',
+    paddingBottom: 10,
+    color: 'green',
+  },
+
+  emailValidationText: {
+    color: 'red',
+    marginBottom: 10,
+  },
+
+  button: {
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#8d71ad',
+    margin: 10,
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 18,
+  },
+});
