5832460b6528d313d8734dcf5b61fb7bd78f87b4
AshleyDowie <67585778+AshleyDowie@users.noreply.github.com>
2022-12-10 20:10:12 +1100

    146 customer pov] onboarding journey meal planning frontend (#13)
    * 209-Create-React-Component-For-Filter-Chips
    
    - Created a new component that is visually the same as the filter chips in figma
    - Brought across images that were exported from figma that will likely need to be used elsewhere too.
    - Import google fonts.
    
    * 146-Customer-POV]-Onboarding-Journey-Meal-Planning-Frontend
    
    - Changed meal planning page design to match figma visually.
    - Implemented button response and functionality.
    
    * 209-Create-React-Component-For-Filter-Chips
    
    - Added new fonts to App.js
    
    * 146-Customer-POV]-Onboarding-Journey-Meal-Planning-Frontend
    
    - Slight tweeks in response to code review.
    
    * 146-Customer-POV]-Onboarding-Journey-Meal-Planning-Frontend
    
    - Fixed issue with bottom row height changing when unchecking/ checking row above

--- Team-TaskforceX/TTFX_T3_2022/Screens/MealPlanning.js
+++ Team-TaskforceX/TTFX_T3_2022/Screens/MealPlanning.js
@@ -1,160 +1,266 @@
+import React, {useState} from 'react';
 import {
-  Pressable,
-  StyleSheet,
   Text,
+  StyleSheet,
+  Image,
   View,
-  TouchableHighlight,
-  Dimensions,
-  SafeAreaView,
-  TouchableOpacity,
+  Pressable,
   FlatList,
-} from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
-import { Searchbar } from "react-native-paper";
+  Dimensions,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {FilterChip} from './Components/FilterChip';
 
-const SCREENHEIGHT = Dimensions.get("window").height;
-const SCREENWIDTH = Dimensions.get("window").width;
+const windowWidth = Dimensions.get('window').width;
 
-const DATA = [
-  { id: "1", title: "None" },
-  { id: "2", title: "Breakfast" },
-  { id: "3", title: "Lunch" },
-  { id: "4", title: "Dinner" },
-  { id: "5", title: "Dessert" },
-];
+const MealPlanning = () => {
+  const navigation = useNavigation();
+  const [none, setNone] = useState(true);
+  const [breakfast, setBreakfast] = useState(false);
+  const [lunch, setLunch] = useState(false);
+  const [dinner, setDinner] = useState(false);
+  const [desert, setDesert] = useState(false);
 
-export default function TodaysPlan({ navigation }) {
-  // const [selectedId, setSelectedId] = useState(null);
-  //   const [allergy, setAllergy] = useState("")
+  const DATA = [
+    {
+      label: 'None',
+      checked: none,
+      getData: {getName},
+      invisible: false,
+    },
+    {
+      label: 'Breakfast',
+      checked: breakfast,
+      getData: {getName},
+      invisible: false,
+    },
+    {
+      label: 'Lunch',
+      checked: lunch,
+      getData: {getName},
+      invisible: false,
+    },
+    {
+      label: 'Dinner',
+      checked: dinner,
+      getData: {getName},
+      invisible: false,
+    },
+    {
+      label: 'Desert',
+      checked: desert,
+      getData: {getName},
+      invisible: false,
+    },
+    {
+      label: 'Invisible',
+      checked: false,
+      invisible: true,
+    },
+  ];
+
+  const Item = ({label, checked, invisible}) => (
+    <FilterChip
+      label={label}
+      checked={checked}
+      getData={getName}
+      invisible={invisible}
+    />
+  );
+
+  const renderItem = ({item}) => (
+    <Item
+      label={item.label}
+      checked={item.checked}
+      getData={item.getName}
+      invisible={item.invisible}
+    />
+  );
+
+  function getName(label) {
+    switch (label) {
+      case 'None':
+        {
+          if (none === false) {
+            setBreakfast(false);
+            setLunch(false);
+            setDinner(false);
+            setDesert(false);
+          }
+          if (!none || breakfast || lunch || dinner || desert) {
+            setNone(!none);
+          }
+        }
+        break;
+      case 'Breakfast':
+        {
+          if (breakfast === false) {
+            setNone(false);
+          } else if (!lunch && !dinner && !desert) {
+            setNone(true);
+          }
+          setBreakfast(!breakfast);
+        }
+        break;
+      case 'Lunch':
+        {
+          if (lunch === false) {
+            setNone(false);
+          } else if (!breakfast && !dinner && !desert) {
+            setNone(true);
+          }
+          setLunch(!lunch);
+        }
+        break;
+      case 'Dinner':
+        {
+          if (dinner === false) {
+            setNone(false);
+          } else if (!breakfast && !lunch && !desert) {
+            setNone(true);
+          }
+          setDinner(!dinner);
+        }
+        break;
+      case 'Desert':
+        {
+          if (desert === false) {
+            setNone(false);
+          } else if (!breakfast && !lunch && !dinner) {
+            setNone(true);
+          }
+          setDesert(!desert);
+        }
+        break;
+    }
+  }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Icon
-        name="arrow-left"
-        size={20}
-        color="black"
-        type="entypo"
-        onPress={() => navigation.goBack()}
-      />
-      <View>
-        <Text style={styles.title}>Meal Planning</Text>
-      </View>
-      <Searchbar />
-      <Text style={styles.text}>
+    <View style={styles.mealPlanningView}>
+      <Pressable style={styles.topAppBarPressable}>
+        <Pressable
+          style={styles.leadingIconPressable}
+          onPress={() => navigation.navigate('DailyNutritionPlan')}>
+          <Image
+            style={styles.icon}
+            resizeMode="cover"
+            source={require('../assets/images/leadingicon.png')}
+          />
+          <Text style={styles.headlineText}>Meal Planning</Text>
+        </Pressable>
+      </Pressable>
+
+      <Text style={styles.paragraphText}>
         Select which recipes our nutritionist will automatically recommend based
         on your preferences and health?
       </Text>
+
       <FlatList
+        style={styles.list}
+        paddingTop={1}
         data={DATA}
+        renderItem={renderItem}
         numColumns={2}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={styles.item}>
-            <Pressable
-              children={({ pressed }) => (
-                <Text
-                  style={[
-                    styles.itemText,
-                    { color: pressed ? "#FF9E00" : "#222" },
-                  ]}
-                >
-                  {item.title}
-                </Text>
-              )}
-            />
-            {/* <Text style={styles.itemText}>{item.title}</Text> */}
-          </View>
+        columnWrapperStyle={{justifyContent: 'space-between'}}
+        ItemSeparatorComponent={() => (
+          <View style={{height: 16, marginHorizontal: 16}} />
         )}
       />
 
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("Permissions")}
-      >
-        <Text style={styles.buttonText}>Continue</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
+      <Pressable
+        style={styles.buttonPressable}
+        onPress={() => navigation.navigate('Permissions')}>
+        <Text style={styles.continueLabel}>Continue</Text>
+      </Pressable>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  paragraphText: {
+    position: 'relative',
+    letterSpacing: -0.2,
+    lineHeight: 24,
+    fontFamily: 'OpenSans_400Regular',
+    color: '#000',
+    textAlign: 'left',
+    height: 74,
+    marginLeft: 16,
+    marginRight: 16,
+    marginTop: 68,
+  },
+  mealPlanningView: {
+    position: 'relative',
+    backgroundColor: '#fffbfe',
     flex: 1,
-    backgroundColor: "white",
-    padding: 30,
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden',
   },
-  title: {
-    fontSize: 25,
-    color: "black",
-    marginTop: 20,
-    marginBottom: 20,
+  column: {
+    flexShrink: 1,
   },
-  text: {
+  buttonPressable: {
+    position: 'absolute',
+    bottom: 32,
+    left: 16,
+    borderRadius: 100,
+    backgroundColor: '#8273a9',
+    width: windowWidth - 32,
+    overflow: 'hidden',
+    flexDirection: 'column',
+    paddingHorizontal: 24,
+    paddingVertical: 10,
+    boxSizing: 'border-box',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  continueLabel: {
+    position: 'relative',
     fontSize: 16,
-    marginBottom: 30,
-    marginTop: 20,
-    color: "black",
+    letterSpacing: 0.1,
+    lineHeight: 20,
+    fontWeight: '700',
+    fontFamily: 'OpenSans_400Regular',
+    color: '#fff',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
-  button: {
-    backgroundColor: "#8d71ad",
-    height: 55,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 25,
-    top: 10,
-    marginBottom: 50,
+  leadingIconPressable: {
+    position: 'relative',
+    marginTop: 52,
+    marginLeft: 16,
+    width: 24,
+    height: 24,
+  },
+  icon: {
+    width: '100%',
+    height: '100%',
+  },
+  headlineText: {
+    position: 'absolute',
+    top: 36,
+    left: 0,
+    fontSize: 24,
+    lineHeight: 32,
+    fontFamily: 'OpenSans_400Regular',
+    color: '#000',
+    textAlign: 'left',
+    display: 'flex',
+    alignItems: 'center',
+    width: 328,
   },
 
-  buttonText: {
-    fontSize: 18,
-    color: "white",
-    fontWeight: "bold",
-  },
-
-  item: {
-    marginTop: 10,
-    // backgroundColor: 'green',
-    borderColor: "black",
-    borderWidth: 1,
-    maxWidth: SCREENWIDTH / 2 - 40,
-    padding: 10,
-    alignItems: "center",
-    borderRadius: 10,
-    justifyContent: "space-around",
-    margin: 5,
-    flex: 0.5,
-  },
-  itemText: {
-    color: "black",
-    // fontFamily: 'Times',
+  list: {
+    position: 'relative',
+    marginTop: 25,
+    marginLeft: 8,
+    marginRight: 8,
   },
 });
 
-// import { StatusBar } from "expo-status-bar";
-// import { Button, StyleSheet, Text, View } from "react-native";
-
-// export default function MealPlanning({ navigation }) {
-//   return (
-//     <View style={styles.container}>
-//       <View style={{ marginLeft: 10, marginTop: 30 }}>
-//         <Text style={{ fontWeight: "bold", fontSize: 30 }}>Meal Planning</Text>
-//       </View>
-//       <Button
-//         title="Continue"
-//         onPress={() => navigation.navigate("Permissions")}
-//       />
-//       <Button title="Exit" onPress={() => navigation.navigate("LandingPage")} />
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+export default MealPlanning;
