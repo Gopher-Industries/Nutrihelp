de4d782ab11f143a3417d9e2f368e9fc2071bcb3
AndrewTTFX <118961659+AndrewTTFX@users.noreply.github.com>
2022-12-13 20:40:28 +1100

    103-Start-Screen

--- Team-TaskforceX/TTFX_T3_2022/App.js
+++ Team-TaskforceX/TTFX_T3_2022/App.js
@@ -38,22 +38,6 @@ import {
   OpenSans_800ExtraBold_Italic,
   } from "@expo-google-fonts/open-sans";
 
-import {
-  useFonts,
-  OpenSans_300Light,
-  OpenSans_400Regular,
-  OpenSans_500Medium,
-  OpenSans_600SemiBold,
-  OpenSans_700Bold,
-  OpenSans_800ExtraBold,
-  OpenSans_300Light_Italic,
-  OpenSans_400Regular_Italic,
-  OpenSans_500Medium_Italic,
-  OpenSans_600SemiBold_Italic,
-  OpenSans_700Bold_Italic,
-  OpenSans_800ExtraBold_Italic,
-} from "@expo-google-fonts/open-sans";
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
