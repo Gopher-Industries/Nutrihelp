fb54154ee687dc5650830fdf03da14a0957ba32e
AndrewTTFX <118961659+AndrewTTFX@users.noreply.github.com>
2022-12-15 07:28:35 +1100

    150-Onboarding-2FA (#19)
    Summary of changes:
    
    -Created entire screen to match wireframe design. (layout, margins, fonts, colours, buttons, input fields).
    -Added comments.
    -Confirmed the continue button flows to "Profile"
    -Confirmed back arrow returns to "Create Account".
    
    Testing Instructions:
    
    -Confirm the continue button flows to "Profile".
    -Confirm back arrow returns to "Create Account".
    -Test 2FA input field.

--- Team-TaskforceX/TTFX_T3_2022/Screens/Authentication.js
+++ Team-TaskforceX/TTFX_T3_2022/Screens/Authentication.js
@@ -1,15 +1,50 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { TextInput as RNPTextInput } from "react-native-paper";
+
+const SCREENHEIGHT = Dimensions.get("window").height;
+const SCREENWIDTH = Dimensions.get("window").width;
 
 export default function Authentication({ navigation }) {
   return (
     <View style={styles.container}>
-      <View style={{ marginLeft: 10, marginTop: 30 }}>
-        <Text style={{ fontWeight: "bold", fontSize: 30 }}>Authentication</Text>
+      <Icon //Back arrow
+        style={styles.backArrow}
+        name="arrow-left"
+        size={20}
+        color="black"
+        type="entypo"
+        onPress={() => navigation.goBack()}
+      />
+      <View>
+        <Text style={styles.title}>Two Factor Authentication</Text>
+        <Text style={styles.textOne}>Protecting your information is very important to NutriHelp</Text>
+        <Text style={styles.textTwo}>Please verify your account by entering the 6-digit code sent to your email address</Text>
       </View>
-      <Button title="Continue" onPress={() => navigation.navigate("Profile")} />
-      <Button title="Back" onPress={() => navigation.goBack()} />
-      <StatusBar style="auto" />
+      <RNPTextInput //2FA field
+        style={styles.twoFactorTextInputRNPTextInput}
+        placeholder="6-Digit Code"
+        label="6-Digit Code"
+        mode="outlined"
+        activeOutlineColor="#8273a9"
+        theme={{
+          fonts: { fontFamily: "OpenSans_400Regular", fontWeight: '600',  },
+          colors: {text: "black"},
+        }}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Profile")}
+      >
+      <Text style={styles.buttonText}>Verify Code</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,8 +52,80 @@ export default function Authentication({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#FFFBFE",
+    padding: 16,
+  },
+
+  //Back Arrow
+  backArrow: {
+    marginTop: 52,
+  },
+
+  //Main Title
+  title: { 
+    fontSize: 24,
+    fontFamily: "OpenSans_400Regular",
+    color: "black",
+    marginTop: 32,
+    lineHeight: 32,
+  },
+
+  //2FA field
+  twoFactorTextInputRNPTextInput: {
+    borderRadius: 4,
+    borderColor: "black",
+    borderStyle: "solid",
+    width: 361,
+    height: 56,
+    backgroundColor: "#FFFBFE",
+    marginTop: -150,
+  },
+
+  //Paragraph One
+  textOne: {
+    marginTop: 40,
+    marginBottom: SCREENHEIGHT / 4,
+    color: "black",
+    fontSize: 16,
+    letterSpacing: 0.1,
+    lineHeight: 20,
+    fontWeight: '600',
+    fontFamily: 'OpenSans_400Regular',
+  },
+
+   //Paragraph Two
+   textTwo: {
+    marginTop: -150,
+    marginBottom: SCREENHEIGHT / 4,
+    color: "black",
+    fontSize: 16,
+    letterSpacing: 0.1,
+    lineHeight: 20,
+    fontWeight: '600',
+    fontFamily: 'OpenSans_400Regular',
+  },
+
+  //Verify code button
+  button: {
+    borderRadius: 100,
+    height: 40,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#8d71ad",
+    marginTop: 273,
+  },
+
+  //Verify code button text
+  buttonText: {
+    fontSize: 16,
+    letterSpacing: 0.1,
+    lineHeight: 20,
+    fontWeight: '700',
+    fontFamily: 'OpenSans_400Regular',
+    color: '#fff',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
+
