ea3e9ec950e135cc085f2beeb8b3f2bcda137dd1
NR <89783021+deegle0-0@users.noreply.github.com>
2023-03-31 13:50:36 +1100

    Update App.js
    Removing excess code

--- Team-TaskforceX/TTFX_T3_2022/App.js
+++ Team-TaskforceX/TTFX_T3_2022/App.js
@@ -23,24 +23,11 @@ import TodaysPlan from "./Screens/TodaysPlan";
 import NutritionalReport from "./Screens/NutritionalReport";
 import LogIn from "./Screens/LogIn";
 
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 
-  let [fontsLoaded] = useFonts({
-    OpenSans_300Light,
-    OpenSans_400Regular,
-    OpenSans_500Medium,
-    OpenSans_600SemiBold,
-    OpenSans_700Bold,
-    OpenSans_800ExtraBold,
-    OpenSans_300Light_Italic,
-    OpenSans_400Regular_Italic,
-    OpenSans_500Medium_Italic,
-    OpenSans_600SemiBold_Italic,
-    OpenSans_700Bold_Italic,
-    OpenSans_800ExtraBold_Italic,
-    });
   return (
     <NavigationContainer>
       <Stack.Navigator>
