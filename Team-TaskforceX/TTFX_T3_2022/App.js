07342c1b49ee93595978b1f095a7b80dc0652067
NR <89783021+deegle0-0@users.noreply.github.com>
2023-03-31 13:52:36 +1100

    Deleting some unnecessary lines of code  (#31)
    * Delete .gitignore
    
    * Update App.js
    
    Deleting code that is not being used by current version of application
    
    * Update App.js
    
    Removing excess code

--- Team-TaskforceX/TTFX_T3_2022/App.js
+++ Team-TaskforceX/TTFX_T3_2022/App.js
@@ -22,41 +22,12 @@ import Notifications from "./Screens/Notifications";
 import TodaysPlan from "./Screens/TodaysPlan";
 import NutritionalReport from "./Screens/NutritionalReport";
 import LogIn from "./Screens/LogIn";
-import {
-  useFonts,
-  OpenSans_300Light,
-  OpenSans_400Regular,
-  OpenSans_500Medium,
-  OpenSans_600SemiBold,
-  OpenSans_700Bold,
-  OpenSans_800ExtraBold,
-  OpenSans_300Light_Italic,
-  OpenSans_400Regular_Italic,
-  OpenSans_500Medium_Italic,
-  OpenSans_600SemiBold_Italic,
-  OpenSans_700Bold_Italic,
-  OpenSans_800ExtraBold_Italic,
-  } from "@expo-google-fonts/open-sans";
-  
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 
-  let [fontsLoaded] = useFonts({
-    OpenSans_300Light,
-    OpenSans_400Regular,
-    OpenSans_500Medium,
-    OpenSans_600SemiBold,
-    OpenSans_700Bold,
-    OpenSans_800ExtraBold,
-    OpenSans_300Light_Italic,
-    OpenSans_400Regular_Italic,
-    OpenSans_500Medium_Italic,
-    OpenSans_600SemiBold_Italic,
-    OpenSans_700Bold_Italic,
-    OpenSans_800ExtraBold_Italic,
-    });
   return (
     <NavigationContainer>
       <Stack.Navigator>
