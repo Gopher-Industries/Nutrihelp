bee186670b6ac80d171277e89a2fb0e5c649c2c5
aravind9800 <aravindmukkala98@gmail.com>
2023-09-19 16:33:57 +1000

    Database connection API

---
+++ backend/db.js
@@ -0,0 +1,19 @@
+// db.js
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'yoMysql@localhost:3306',
+  user: 'root',
+  password: 'Aravind@98',
+  database: 'nutrihelp',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database: ', err);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+module.exports = db;
