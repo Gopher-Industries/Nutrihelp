92b8623c07370a081978eedf3988c1b10632beb1
JingzeDu <54390099+JingzeDu@users.noreply.github.com>
2023-09-27 20:24:27 +1000

    Add files via upload

--- Website/app.js
+++ Website/app.js
@@ -1,22 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-let dbConnect = require("./dbConnect");
-
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Routes
-
-//Routes
-app.use("/", require("./routes/auth"));
-
+// Define routes and middleware here
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
