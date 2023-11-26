59250c4ee877cfa17736639a91ea8e7e2934c86d
petertieu <tieu.peter.v@gmail.com>
2023-11-26 15:13:26 +1100

    Instant Chatbot

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -1,87 +1,3 @@
-// import React, { useContext, useEffect } from 'react';
-// import PreferencesComponent from './components/pref-dis-health';
-// import Login from './components/Login/Login';
-// import Landing from './components/Landing';
-
-
-// function App() {
-//   return (
-//     <div>
-//       {/* <PreferencesComponent /> */}
-//       <Login />
-//     </div>
-//   );
-// }
-
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'semantic-ui-css/semantic.min.css'
-// import './App.css';
-// import { Routes, Route, Navigate } from 'react-router-dom'
-// // import Login from './routes/Login/Login'
-// import NavFooterAndFooterLoggedIn from './components/navigation_bars_and_footers/logged_in/NavBarAndFooterLoggedIn';
-// import NavFooterAndFooterNotLoggedIn from './components/navigation_bars_and_footers/not_logged_in/NavBarAndFooterNotLoggedIn';
-
-
-
-// function App() {
-
-//   console.log("Check current user in App.js")
-
-//   return (
-//     <Routes>
-//       {/* Define the routes under the path '/' */}
-//       <Route path='/' element={<NavFooterAndFooterNotLoggedIn />}>
-
-//         {/* Let the Login page be accessible by the path '/' alone */}
-//         <Route index element={<Login />} />
-
-//         {/* List all the possible routes of under the path '/' (below): */}
-//         <Route path='landing' element={<Landing />} />
-//       </Route>
-
-
-//       {/* Define the routes under the path '/user' */}
-//       <Route path='/user' element={<NavFooterAndFooterLoggedIn />}>
-
-//         {/* Let the HomePage page be accessible by the path '/user' alone */}
-//         <Route index element={<HomePage />} />
-
-//         {/* List all the possible routes of under the path '/user' (below): */}
-//         <Route path='landing' element={<Landing />} />
-//       </Route>
-
-//     </Routes >
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React, { useContext, useEffect } from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
@@ -96,6 +12,19 @@ import ForgotPassword from './routes/ForgotPassword/ForgotPassword';
 
 
 function App() {
+
+  useEffect(() => {
+    (function (d, m) {
+      var kommunicateSettings =
+        { "appId": "6ba050337f189922ca5b3c0e644a05e", "popupWidget": true, "automaticChatOpenOnNavigation": true };
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+  }, []);  // Empty dependency array ensures this runs only once after component mount
+
+
   return (
     <Router>
       <Routes>
