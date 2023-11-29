83f3a5e126d46b600e703eee9139f53a0511dbba
Anshuman Bishnoi <anshumanbishnoi0009@gmail.com>
2023-11-29 16:13:43 +1100

    added user info page

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PreferencesComponent from './components/pref-dis-health';
-
+import UserInfo from './components/Userinfo';
 
 function App() {
     return (
       <div>
-        <PreferencesComponent />
+        {/* <PreferencesComponent />  */}
+        <UserInfo />
       </div>
     );
   }
