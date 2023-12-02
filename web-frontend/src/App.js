1632d26643f5a04b01d5f4de62cec68451798ddc
Darshan <109759629+DARSHcverse@users.noreply.github.com>
2023-12-02 17:21:41 +1100

    Sign up
    This is a sign up page, which allows the user to create a new account.

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PreferencesComponent from './components/pref-dis-health';
 import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 import Land from './components/Landing';
 import Chat2 from './components/chtgptIMP';
 
 function App() {
     return (
       <div>
-        <Land/>
-        <Chat2/>
+        <SignUp/>
+        {/* <Land/> */}
+        {/* <Chat2/> */}
         {/* <PreferencesComponent /> */}
         {/* <SignIn/> */}
       </div>
