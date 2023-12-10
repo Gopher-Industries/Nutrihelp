377b7ad4c52de7eb769d0312fc0847e3dc0c535d
Denish-Sen <141387458+Denish-Sen@users.noreply.github.com>
2023-12-10 18:41:11 +1100

    Dietry requirements - Nutrition-Snacks (#82)
    * [diet-changes]
    
    * [Nutrition-Snacks]
    
    * [Nutrition-Snacks]
    
    * [Nutrition-Snack]
    
    ---------
    
    Co-authored-by: Peter Tieu <tieu.peter.v@gmail.com>

---
+++ web-frontend/src/Nutrition-Snacks.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import '../styles/auth.css';
+
+
+        <><h2>Hard-Boiled Eggs</h2><p>Hard-boiled eggs are a quick grab-and-go snack that is also a great protein source. The eggs can be prepped in advance and will keep in the refrigerator for about 7 days. You can enjoy them plain, or with a sprinkle of salt and pepper.</p><div data-test="image-block-inline-outer-wrapper">
+            <figure>
+                <div data-animation-role="image">
+                    <div>
+                        <p><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/mixednuts.jpg" alt="mixednuts.jpg" /></noscript><img decoding="async" src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%200%200'%3E%3C/svg%3E" alt="mixednuts.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/mixednuts.jpg" data-image-dimensions="702x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="614386325d7fd84c28a6daf7" data-type="image" data-lazy-src="https://chefsforseniors.com/wp-content/uploads/2022/12/mixednuts.jpg" /><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/mixednuts.jpg" alt="mixednuts.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/mixednuts.jpg" data-image-dimensions="702x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="614386325d7fd84c28a6daf7" data-type="image" /></noscript></p>
+                    </div>
+                </div>
+            </figure>
+        </div><h2>Unsalted Mixed Nuts</h2><p>Mixed nuts are an easy, no-prep needed snack. Choose a variety of nuts with heart-healthy fats and protein such as almonds, cashews, pistachios, walnuts and brazil nuts. You can also mix in dried fruit such as cranberries or raisins for a variety in taste and texture.</p><div data-test="image-block-inline-outer-wrapper">
+                <figure>
+                    <div data-animation-role="image">
+                        <div>
+                            <p><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/smoothie.jpg" alt="smoothie.jpg" /></noscript><img decoding="async" src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%200%200'%3E%3C/svg%3E" alt="smoothie.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/smoothie.jpg" data-image-dimensions="1980x1320" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="614386490d3ea81b55bc2f62" data-type="image" data-lazy-src="https://chefsforseniors.com/wp-content/uploads/2022/12/smoothie.jpg" /><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/smoothie.jpg" alt="smoothie.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/smoothie.jpg" data-image-dimensions="1980x1320" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="614386490d3ea81b55bc2f62" data-type="image" /></noscript></p>
+                        </div>
+                    </div>
+                </figure>
+            </div><h2>Smoothies</h2><p>Fruit smoothies are nutrient rich and delicious, and the best part is: you can mix in other ingredients like spinach, protein powder, etc. and barely taste it! There are hundreds of smoothie combinations to try- just use your favorite frozen fruits, milk or yogurt, and any other additions!</p><div data-test="image-block-inline-outer-wrapper">
+                <figure>
+                    <div data-animation-role="image">
+                        <div>
+                            <p><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/granola.jpg" alt="granola.jpg" /></noscript><img decoding="async" src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%200%200'%3E%3C/svg%3E" alt="granola.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/granola.jpg" data-image-dimensions="936x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="61438664f38f8b113ff2cbe4" data-type="image" data-lazy-src="https://chefsforseniors.com/wp-content/uploads/2022/12/granola.jpg" /><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/granola.jpg" alt="granola.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/granola.jpg" data-image-dimensions="936x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="61438664f38f8b113ff2cbe4" data-type="image" /></noscript></p>
+                        </div>
+                    </div>
+                </figure>
+            </div><h2>Homemade Granola</h2><p>Homemade granola is a great alternative to store bought granola, because you can control how much sugar is added and customize it to your liking. You can use a base of dried oats, dried fruit, nuts/seeds and add in honey or maple syrup for a touch of sweetness. Granola can be enjoyed atop yogurt, eaten like cereal or even enjoyed by the handful!</p><div data-test="image-block-inline-outer-wrapper">
+                <figure>
+                    <div data-animation-role="image">
+                        <div>
+                            <p><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/veggieshummus.jpg" alt="veggieshummus.jpg" /></noscript><img decoding="async" src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%200%200'%3E%3C/svg%3E" alt="veggieshummus.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/veggieshummus.jpg" data-image-dimensions="720x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="61438679f38f8b113ff2cd17" data-type="image" data-lazy-src="https://chefsforseniors.com/wp-content/uploads/2022/12/veggieshummus.jpg" /><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/veggieshummus.jpg" alt="veggieshummus.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/veggieshummus.jpg" data-image-dimensions="720x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="61438679f38f8b113ff2cd17" data-type="image" /></noscript></p>
+                        </div>
+                    </div>
+                </figure>
+            </div><h2> Veggies w/ Hummus</h2><p>Raw vegetables with a delicious dip are a great way to get your servings of veggies in a day, and also a great source of fiber. Try carrots, cucumbers, broccoli, cauliflower, etc. Hummus is a tasty alternative to store-bought dips and dressings- and can be found in a variety of flavors like garlic, roasted red pepper or sun-dried tomato.</p><div data-test="image-block-inline-outer-wrapper">
+                <figure>
+                    <div data-animation-role="image">
+                        <div>
+                            <p><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/apples.jpg" alt="apples.jpg" /></noscript><img decoding="async" src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%200%200'%3E%3C/svg%3E" alt="apples.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/apples.jpg" data-image-dimensions="624x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="61438691cbf1130059bc1a3e" data-type="image" data-lazy-src="https://chefsforseniors.com/wp-content/uploads/2022/12/apples.jpg" /><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/apples.jpg" alt="apples.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/apples.jpg" data-image-dimensions="624x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="61438691cbf1130059bc1a3e" data-type="image" /></noscript></p>
+                        </div>
+                    </div>
+                </figure>
+            </div><h2>Sauteed Apples w/Cinnamon</h2><p>Enjoy the taste of fall with this warming snack all year long! Cooked fruit is very easily digested, and is a great option for those on a soft-foods diet. It’s simple- cut an apple into ½ inch pieces, sauté with butter or coconut oil and a sprinkle of cinnamon until soft. Pears also work well as an alternative.</p><div data-test="image-block-inline-outer-wrapper">
+                <figure>
+                    <div data-animation-role="image">
+                        <div>
+                            <p><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/almonds.jpg" alt="almonds.jpg" /></noscript><img decoding="async" src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%200%200'%3E%3C/svg%3E" alt="almonds.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/almonds.jpg" data-image-dimensions="720x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="614386ad1a64b1606d169e08" data-type="image" data-lazy-src="https://chefsforseniors.com/wp-content/uploads/2022/12/almonds.jpg" /><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/almonds.jpg" alt="almonds.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/almonds.jpg" data-image-dimensions="720x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="614386ad1a64b1606d169e08" data-type="image" /></noscript></p>
+                        </div>
+                    </div>
+                </figure>
+            </div><h2>Dark Chocolate Covered Almonds</h2><p>This crunchy snack gives us a healthy boost of heart-healthy fats from the almonds and a boost of antioxidants from the dark chocolate. Just a hand-full a day keeps the sugar cravings away!</p><div data-test="image-block-inline-outer-wrapper">
+                <figure>
+                    <div data-animation-role="image">
+                        <div>
+                            <p><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/fishcracker.jpg" alt="fishcracker.jpg" /></noscript><img decoding="async" src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%200%200'%3E%3C/svg%3E" alt="fishcracker.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/fishcracker.jpg" data-image-dimensions="720x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="614386c2edda12718241d55c" data-type="image" data-lazy-src="https://chefsforseniors.com/wp-content/uploads/2022/12/fishcracker.jpg" /><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/fishcracker.jpg" alt="fishcracker.jpg" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/fishcracker.jpg" data-image-dimensions="720x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="614386c2edda12718241d55c" data-type="image" /></noscript></p>
+                        </div>
+                    </div>
+                </figure>
+            </div><h2>Sardines/Canned Salmon</h2><p>Fish like salmon and sardines are packed with omega-3 fatty acids. They also have an impressive amount of vitamin-D and calcium, which is beneficial for bone health. Make sure to look for canned fish in water or olive oil. Enjoy on top of crackers for a surprisingly satisfying snack.</p><div data-test="image-block-inline-outer-wrapper">
+                <figure>
+                    <div data-animation-role="image">
+                        <div>
+                            <p><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/cottagecheese.jpg" alt="" /></noscript><img decoding="async" src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%200%200'%3E%3C/svg%3E" alt="" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/cottagecheese.jpg" data-image-dimensions="720x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="614386d66a96ec0480a37162" data-type="image" data-lazy-src="https://chefsforseniors.com/wp-content/uploads/2022/12/cottagecheese.jpg" /><noscript><img decoding="async" src="https://chefsforseniors.com/wp-content/uploads/2022/12/cottagecheese.jpg" alt="" data-image="https://chefsforseniors.com/wp-content/uploads/2022/12/cottagecheese.jpg" data-image-dimensions="720x468" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="614386d66a96ec0480a37162" data-type="image" /></noscript></p>
+                        </div>
+                    </div>
+                </figure>
+            </div></>  
+        
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
