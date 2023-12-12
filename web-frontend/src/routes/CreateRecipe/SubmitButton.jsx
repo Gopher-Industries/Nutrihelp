2f560e0e3d351dfacce5f8e2b2a65454dabe0632
Kaarunya9 <140236918+Kaarunya9@users.noreply.github.com>
2023-12-13 00:00:05 +1100

    App.js changes (#91)
    Changes done on app.js (recipe)

--- web-frontend/src/routes/CreateRecipe/SubmitButton.jsx
+++ web-frontend/src/routes/CreateRecipe/SubmitButton.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 import './SubmitButton.css';
+import { Link } from 'react-router-dom';
 
 // Button to submit a form
 const SubmitButton = ({ text, onSubmit, disabled }) => {
 
     return (
-        <div className='button-div'>
-            <Button onClick={onSubmit} disabled={disabled}>
-                {text}
-            </Button>
+        <div className='button-div'><Link to={'/recipe'}><Button onClick={onSubmit} disabled={disabled}>
+        {text}
+    </Button>
+    </Link>
+            
         </div>
     )
 }
